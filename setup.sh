bcd210f36bd244a5c3d5f540814ce8a5f23c6d2b
Stefan Arentz <stefan@arentz.ca>
2013-01-30 16:12:47 -0500

    Added script easily set things up for development

---
+++ setup.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+set -x
+
+#
+# make.sh <clone|setup|run-plugin-service|run-task-engine>
+#
+# This script is really just for development only. It makes it easier to
+# checkout the depend projects and to set them up in a virtualenv.
+#
+
+PROJECTS="core nmap-plugin"
+
+if [ "$(id -u)" == "0" ]; then
+    echo "abort: cannot run as root."
+    exit 1
+fi
+
+if [ ! `which virtualenv` ]; then
+    echi "abort: no virtualenv found"
+fi
+
+if [ ! `which python2.7` ]; then
+    echi "abort: no python2.7 found"
+fi
+
+if [ ! -z "$VIRTUAL_ENV" ]; then
+    echo "abort: cannot run from an existing virtual environment"
+    exit 1
+fi
+
+case $1 in
+    clone)
+        for project in $PROJECTS; do
+            if [ ! -d "minion-$project" ]; then
+                git clone "https://github.com/st3fan/minion-$project" || exit 1
+            fi
+        done
+        ;;
+    develop)
+        # Create our virtualenv
+        if [ ! -d "env" ]; then
+            virtualenv -p python2.7 --no-site-packages env || exit 1
+        fi
+        # Activate our virtualenv
+        source env/bin/activate
+        for project in $PROJECTS; do
+            if [ -x "minion-$project/setup.sh" ]; then
+				(cd "minion-$project"; "./setup.sh" develop || exit 1)
+            fi
+        done
+        ;;
+    run-plugin-service)
+        source env/bin/activate
+        minion-core/plugin-service/scripts/minion-plugin-service
+        ;;
+    run-task-engine)
+        source env/bin/activate
+        minion-core/task-engine/scripts/minion-task-engine
+        ;;
+esac
